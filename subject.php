b4fd7c89c3ebbda0cc2f872c5547f9172142b751
Bruno Žitković <brunozitkovic@gmail.com>
2020-12-27 22:08:26 +0100

    added insertion of new subject and listing of existing ones

--- subject.php
+++ subject.php
@@ -1,3 +1,47 @@
+<?php
+  include_once './conn.php';
+
+  if(isset($_POST["submit"])){
+    
+    $nazivPredmeta = pg_escape_string( $_POST['naziv_predmeta']);
+    $brojEctsa = pg_escape_string( $_POST['broj_ectsa']);
+    $idNastavnika = pg_escape_string( $_POST['nastavnik']);
+    $idDvorane = pg_escape_string( $_POST['dvorana']);
+    $dan = pg_escape_string( $_POST['dan']);
+    $vrijemeOd = pg_escape_string( $_POST['vrijeme_od']);
+    $vrijemeDo = pg_escape_string( $_POST['vrijeme_do']);
+    $opisPredmeta = pg_escape_string( $_POST['opis_predmeta']);
+    
+    $query = "INSERT INTO predmet (naziv, ects, opis) VALUES ('$nazivPredmeta', '$brojEctsa', '$opisPredmeta') RETURNING id_predmeta AS id";
+    $rezultat = pg_query($dbconn, $query);
+    $idPredmeta = pg_fetch_row($rezultat);
+
+    $query2 = "INSERT INTO vrijeme (dan, vrijeme_od, vrijeme_do) VALUES ('$dan', '$vrijemeOd', '$vrijemeDo') RETURNING id_vremena AS id";
+    $rezultat = pg_query($dbconn, $query2);
+    $idVremena = pg_fetch_row($rezultat);
+
+   
+    $idVremena = intval($idVremena[0]);
+    $idPredmeta = intval($idPredmeta[0]);
+
+    $query3 = "INSERT INTO traje (id_vremena, id_predmeta) VALUES ('$idVremena', '$idPredmeta')";
+    $rezultat = pg_query($dbconn, $query3);
+
+    $query4 = "INSERT INTO predaje (id_predmeta, id_nastavnika) VALUES ('$idPredmeta', '$idNastavnika')";
+    $rezultat = pg_query($dbconn, $query4);
+
+    $query5 = "INSERT INTO se_izvodi (id_predmeta, id_dvorane) VALUES ('$idPredmeta', '$idDvorane')";
+    $rezultat = pg_query($dbconn, $query5);
+   
+  }
+
+  $rezultatPredmeta = pg_query('SELECT * FROM predmet');
+
+  $predmeti =  pg_fetch_all($rezultatPredmeta);
+ 
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -23,6 +67,9 @@
       </div>
     </div>
     <div class="subject-cards">
+      <?php
+          foreach( $predmeti as $predmet) {                     
+      ?>
       <div class="info-box">
         <div class="info-box__header">
           <figure class="info-box__subheader-figure">
@@ -32,7 +79,7 @@
               alt="club-image"
             />
           </figure>
-          <h2 class="info-box__title">Teorija baza podataka</h2>
+          <h2 class="info-box__title"><?php {echo $predmet['naziv'];} ?></h2>
         </div>
 
         <div class="info-box__subheader">
@@ -45,223 +92,30 @@
               />
             </figure>
             <b><span class="info-box__subheader-box-text">ECTS</span></b>
-            <span class="info-box__subheader-box-text">5</span>
+            <span class="info-box__subheader-box-text"><?php {echo $predmet['ects'];} ?></span>
           </div>
         </div>
         <p class="info-box__about">
           <b
             ><span class="info-box__subheader-box-text"
               >Opis predmeta <br /><br />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo,
-              omnis. Culpa a debitis cumque laborum nam enim quos recusandae
-              ullam molestiae expedita saepe illum pariatur nihil, vero et
-              minima quidem.
+              <?php {echo $predmet['opis'];} ?>
             </span></b
           >
         </p>
         <b>
           <div class="info-box__footer">
             <a
-              href="https://www.juventus.com/en/"
-              target="_blank"
+              href="#"
               class="info-box__btn-join"
               >Službena stranica</a
             >
           </div>
         </b>
       </div>
-
-      <div class="info-box">
-        <div class="info-box__header">
-          <figure class="info-box__subheader-figure">
-            <img
-              class="info-box__subheader-img"
-              src="https://cdn.onlinewebfonts.com/svg/img_341860.png"
-              alt="club-image"
-            />
-          </figure>
-          <h2 class="info-box__title">Teorija baza podataka</h2>
-        </div>
-
-        <div class="info-box__subheader">
-          <div class="info-box__subheader-box">
-            <figure class="info-box__subheader-figure">
-              <img
-                class="info-box__subheader-img"
-                src="https://static.thenounproject.com/png/2479138-200.png"
-                alt="country-image"
-              />
-            </figure>
-            <b><span class="info-box__subheader-box-text">ECTS</span></b>
-            <span class="info-box__subheader-box-text">5</span>
-          </div>
-        </div>
-        <p class="info-box__about">
-          <b
-            ><span class="info-box__subheader-box-text"
-              >Opis predmeta <br /><br />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo,
-              omnis. Culpa a debitis cumque laborum nam enim quos recusandae
-              ullam molestiae expedita saepe illum pariatur nihil, vero et
-              minima quidem.
-            </span></b
-          >
-        </p>
-        <b>
-          <div class="info-box__footer">
-            <a
-              href="https://www.juventus.com/en/"
-              target="_blank"
-              class="info-box__btn-join"
-              >Službena stranica</a
-            >
-          </div>
-        </b>
-      </div>
-
-      <div class="info-box">
-        <div class="info-box__header">
-          <figure class="info-box__subheader-figure">
-            <img
-              class="info-box__subheader-img"
-              src="https://cdn.onlinewebfonts.com/svg/img_341860.png"
-              alt="club-image"
-            />
-          </figure>
-          <h2 class="info-box__title">Teorija baza podataka</h2>
-        </div>
-
-        <div class="info-box__subheader">
-          <div class="info-box__subheader-box">
-            <figure class="info-box__subheader-figure">
-              <img
-                class="info-box__subheader-img"
-                src="https://static.thenounproject.com/png/2479138-200.png"
-                alt="country-image"
-              />
-            </figure>
-            <b><span class="info-box__subheader-box-text">ECTS</span></b>
-            <span class="info-box__subheader-box-text">5</span>
-          </div>
-        </div>
-        <p class="info-box__about">
-          <b
-            ><span class="info-box__subheader-box-text"
-              >Opis predmeta <br /><br />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo,
-              omnis. Culpa a debitis cumque laborum nam enim quos recusandae
-              ullam molestiae expedita saepe illum pariatur nihil, vero et
-              minima quidem.
-            </span></b
-          >
-        </p>
-        <b>
-          <div class="info-box__footer">
-            <a
-              href="https://www.juventus.com/en/"
-              target="_blank"
-              class="info-box__btn-join"
-              >Službena stranica</a
-            >
-          </div>
-        </b>
-      </div>
-
-      <div class="info-box">
-        <div class="info-box__header">
-          <figure class="info-box__subheader-figure">
-            <img
-              class="info-box__subheader-img"
-              src="https://cdn.onlinewebfonts.com/svg/img_341860.png"
-              alt="club-image"
-            />
-          </figure>
-          <h2 class="info-box__title">Teorija baza podataka</h2>
-        </div>
-
-        <div class="info-box__subheader">
-          <div class="info-box__subheader-box">
-            <figure class="info-box__subheader-figure">
-              <img
-                class="info-box__subheader-img"
-                src="https://static.thenounproject.com/png/2479138-200.png"
-                alt="country-image"
-              />
-            </figure>
-            <b><span class="info-box__subheader-box-text">ECTS</span></b>
-            <span class="info-box__subheader-box-text">5</span>
-          </div>
-        </div>
-        <p class="info-box__about">
-          <b
-            ><span class="info-box__subheader-box-text"
-              >Opis predmeta <br /><br />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo,
-              omnis. Culpa a debitis cumque laborum nam enim quos recusandae
-              ullam molestiae expedita saepe illum pariatur nihil, vero et
-              minima quidem.
-            </span></b
-          >
-        </p>
-        <b>
-          <div class="info-box__footer">
-            <a
-              href="https://www.juventus.com/en/"
-              target="_blank"
-              class="info-box__btn-join"
-              >Službena stranica</a
-            >
-          </div>
-        </b>
-      </div>
-
-      <div class="info-box">
-        <div class="info-box__header">
-          <figure class="info-box__subheader-figure">
-            <img
-              class="info-box__subheader-img"
-              src="https://cdn.onlinewebfonts.com/svg/img_341860.png"
-              alt="club-image"
-            />
-          </figure>
-          <h2 class="info-box__title">Teorija baza podataka</h2>
-        </div>
-
-        <div class="info-box__subheader">
-          <div class="info-box__subheader-box">
-            <figure class="info-box__subheader-figure">
-              <img
-                class="info-box__subheader-img"
-                src="https://static.thenounproject.com/png/2479138-200.png"
-                alt="country-image"
-              />
-            </figure>
-            <b><span class="info-box__subheader-box-text">ECTS</span></b>
-            <span class="info-box__subheader-box-text">5</span>
-          </div>
-        </div>
-        <p class="info-box__about">
-          <b
-            ><span class="info-box__subheader-box-text"
-              >Opis predmeta <br /><br />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo,
-              omnis. Culpa a debitis cumque laborum nam enim quos recusandae
-              ullam molestiae expedita saepe illum pariatur nihil, vero et
-              minima quidem.
-            </span></b
-          >
-        </p>
-        <b>
-          <div class="info-box__footer">
-            <a
-              href="https://www.juventus.com/en/"
-              target="_blank"
-              class="info-box__btn-join"
-              >Službena stranica</a
-            >
-          </div>
-        </b>
-      </div>
+      <?php 
+        }; 
+      ?>
     </div>
     <script src="script.js"></script>
   </body>
