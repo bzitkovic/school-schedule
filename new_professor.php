3f16ed3ba01d6093360f37441fcc782acb4a2ac2
Bruno Žitković <brunozitkovic@gmail.com>
2020-12-30 19:29:26 +0100

    finished CRUD for professors

--- new_professor.php
+++ new_professor.php
@@ -1,3 +1,18 @@
+<?php
+  include_once './conn.php';
+
+  if(isset($_POST["update"])){
+
+    $idNastavnika = $_POST["update"];
+
+    $query = "SELECT * from nastavnik WHERE id_nastavnika = '$idNastavnika' ";
+    $rezultatNastavnika = pg_query($dbconn, $query);
+    $nastavnici = pg_fetch_all($rezultatNastavnika);
+
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -11,21 +26,34 @@
       <h1>Stvori novog nastavnika</h1>
       <form method="POST" action="./professor.php">
         <div class="form-control">
-          <input name="ime_nastavnika" type="text" required />
+          <input name="ime_nastavnika" type="text" value="<?php 
+          if(isset($nastavnici))
+            {echo $nastavnici[0]['ime'];} 
+          ?>"  required />
           <label>Ime profesora</label>
         </div>
 
         <div class="form-control">
-          <input name="prezime_nastavnika" type="text" required />
+          <input name="prezime_nastavnika" type="text" value="<?php 
+          if(isset($nastavnici))
+            {echo $nastavnici[0]['prezime'];} 
+          ?>"  required />
           <label>Prezime profesora</label>
         </div>
 
         <div class="form-control">
-          <input name="email_nastavnika" type="text" required />
+          <input name="email_nastavnika" type="text" value="<?php 
+          if(isset($nastavnici))
+            {echo $nastavnici[0]['email'];} 
+          ?>"  required />
           <label>Email profesora</label>
         </div>
 
-        <button name="submit" class="btn">Stvori nastavnika</button>
+        <?php if(isset($nastavnici)){ ?>
+          <button value="<?php {echo $nastavnici[0]['id_nastavnika'];} ?>" name="update" class="btn">Uredi nastavnika</button>
+        <?php } else { ?>
+          <button name="submit" class="btn">Kreiraj nastavnika</button>
+        <?php } ?>
 
         <p class="text">
           Povratak na popis profesora? <a href="./professor.php">Vrati se</a>
